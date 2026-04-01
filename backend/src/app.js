66a58ef60af9edd4b90d7508fc0203d161712301
javieralvarado <falvarado@lecosy.com.mx>
2026-04-01 14:35:26 -0700

    commit add CRUD actions complete

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,9 @@ app.use(express.json());
 
 /*Routes import*/ 
 import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
 
 /* routes declaration */
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter);
 export default app;
